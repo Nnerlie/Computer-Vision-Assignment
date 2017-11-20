ddedc8a18bb52b09db724e42c3ddc9cf7a2b5ebc
James Neill <jneill542@gmail.com>
2017-11-20 00:30:06 +0000

    Basic evaluation method

--- Script.m
+++ Script.m
@@ -6,7 +6,13 @@
 
 X_train = [ X_manmade_train; X_natural_train];
 Y_train = [ Y_manmade_train; Y_natural_train];
+X_test = [ X_manmade_test; X_natural_test];
+Y_test = [ Y_manmade_test; Y_natural_test];
 
-ic = ImageClassifier(X_train, Y_train, 10).Train();
+clearvars X_manmade_train X_manmade_test Y_manmade_train Y_manmade_test
+clearvars X_natural_train X_natural_test Y_natural_train Y_natural_test
 
-[labels, scores, costs] = ic.Predict(X_train);
+
+ic = ImageClassifier(X_train, Y_train, 3).Train();
+
+[labels, scores, costs] = ic.Evaluate(X_test, Y_test);

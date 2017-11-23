f6eb1edccd8cb45bbac54ee342987e92e2655d1d
jmsnll <jneill542@gmail.com>
2017-11-23 16:48:20 +0000

    Final hand-in

--- Benchmarker.m
+++
@@ -1,6 +0,0 @@
-tiles_x = 96;
-tiles_y = 144;
-
-profile on
-CompositeImage('image.jpg', 'all_images.txt', tiles_x, tiles_y);
-profile viewer
---
+++ evaluate_k.m
@@ -0,0 +1,47 @@
+if exist('X_train.mat', 'file')
+    fprintf('Loading models from disk..\n');
+    load X_train
+    load Y_train
+    load X_test
+    load Y_test
+else
+    fprintf('Loading manmade training data..\n');
+    [X_manmade_train, Y_manmade_train] = ImageClassifier.LoadData('manmade_training.txt');
+    fprintf('Loading manmade test data..\n');
+    [X_manmade_test, Y_manmade_test] = ImageClassifier.LoadData('manmade_test.txt');
+
+    fprintf('Loading natural training data..\n');
+    [X_natural_train, Y_natural_train] = ImageClassifier.LoadData('natural_training.txt');
+    fprintf('Loading natural test data..\n');
+    [X_natural_test, Y_natural_test] = ImageClassifier.LoadData('natural_test.txt');
+
+    fprintf('Combining into the approriate X & Y matrices..\n');
+    X_train = [ X_manmade_train; X_natural_train];
+    Y_train = [ Y_manmade_train; Y_natural_train];
+    X_test = [ X_manmade_test; X_natural_test];
+    Y_test = [ Y_manmade_test; Y_natural_test];
+
+    save X_train
+    save Y_train
+    save X_test
+    save Y_test
+end
+
+clearvars X_manmade_train X_manmade_test Y_manmade_train Y_manmade_test
+clearvars X_natural_train X_natural_test Y_natural_train Y_natural_test
+
+k = 0;
+results = [];
+while(k < 32)
+    k = k + 1;
+    if(mod(k, 25) == 0)
+        disp(k);
+    end
+    tic;
+    ic = ImageClassifier(X_train, Y_train, k).Train();
+    time = toc;
+    
+    [percent, correct, total] = ic.Evaluate(X_test, Y_test);
+    results = [results; k, time, percent];
+end
+csvwrite('linear_classifier.csv', results);
